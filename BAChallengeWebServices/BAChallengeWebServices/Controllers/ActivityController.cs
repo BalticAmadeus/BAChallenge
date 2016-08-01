238c4266c6ceaf43a8193b27935a9d512cf9e070
Studentas <Studentas@STUDENTAS>
2016-08-01 12:10:34 -0700

    Push and Delete

--- BAChallengeWebServices/BAChallengeWebServices/Controllers/ActivityController.cs
+++ BAChallengeWebServices/BAChallengeWebServices/Controllers/ActivityController.cs
@@ -17,12 +17,42 @@ namespace BAChallengeWebServices.Controllers
         {
             _dbContext = new ApplicationDBContext();
         }
-
         public IHttpActionResult Get()
         {
             //Activity act = new Activity { ActivityId = 1, Name = "Begimas", Date = DateTime.Now, RegistrationDate = DateTime.Now, Branch = ActivityBranch.Brain, Status = ActivityStatus.Open, Description = "Blank" };
+            //_dbContext.Activities.Add(act);
+            //_dbContext.SaveChanges();
             var act = _dbContext.Activities.Where(x => x.Status == ActivityStatus.Open);
             return Ok(act);
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            //return Ok();
+            //int id = Int32.Parse(activityId);
+            var activity = _dbContext.Activities.FirstOrDefault(u => u.ActivityId == id);
+            if(activity != null)
+            {
+                _dbContext.Activities.Remove(activity);
+                _dbContext.SaveChanges();
+                return Ok();
+            }
+            return NotFound();
+        }
+        public IHttpActionResult Put(int id,[FromBody]Activity activity)
+        {
+            var selectedRow = _dbContext.Activities.FirstOrDefault(u => u.ActivityId == id);
+            if (selectedRow != null)
+            {
+                selectedRow.Name = activity.Name;
+                selectedRow.Date = activity.Date;
+                selectedRow.RegistrationDate = activity.RegistrationDate;
+                selectedRow.Branch = activity.Branch;
+                selectedRow.Status = activity.Status;
+                selectedRow.Description = activity.Description;
+                _dbContext.SaveChanges();
+                return Ok();
+            }
+            return NotFound();
         }
     }
 }
