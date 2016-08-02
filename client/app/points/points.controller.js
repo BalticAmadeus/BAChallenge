b7cad09130d7e7b409c1de3383be3083c6f1a1cf
vstrimaitis <vstrimaitis@gmail.com>
2016-08-02 23:07:35 +0300

    Revert "Fixed sidebar, made the results table fetch data using angular"
    This reverts commit 07ff46ed59c26714f887c019ba0f0b1ca98464b5.

--- client/app/points/points.controller.js
+++ client/app/points/points.controller.js
@@ -5,15 +5,86 @@
 	angular
 		.module('EventsApp')
 		.controller('PointsController', PointsController);
-    
-	function PointsController () {
+
+	//PointsController.$inject = ['$scope'];
+
+	function PointsController (/*$scope*/) {
+
 		var vm = this;
-        // Pakeist i skaityma is duombazes
-        vm.people = [
-            {"Vardas":"Jonas", "Pavarde":"Jonaitis", "Vieta":"1", "Sport":"2", "Brain":"4", "Games":"4", "Team":"4", "Total":"14"},
-            {"Vardas":"Petras", "Pavarde":"Petraitis", "Vieta":"2", "Sport":"2", "Brain":"2", "Games":"2", "Team":"2", "Total":"8"},
-            {"Vardas":"Ona", "Pavarde":"Onaitiene", "Vieta":"3", "Sport":"1", "Brain":"1", "Games":"1", "Team":"1", "Total":"4"}
-        ];
+        $(document).ready(function () {
+        var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+        isClosed = false;
+
+        trigger.click(function () {
+            hamburger_cross();      
+        });
+
+        function hamburger_cross() {
+
+        if (isClosed == true) {          
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+        } else {   
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+          }
+        }
+  
+        $('[data-toggle="offcanvas"]').click(function () {
+            $('#wrapper').show()
+        });  
+        });
+
 	};
-   
+    $(document).ready(function() {
+    var activeSystemClass = $('.list-group-item.active');
+
+    //something is entered in search form
+    $('#system-search').keyup( function() {
+       var that = this;
+        // affect all table rows on in systems table
+        var tableBody = $('.table-list-search tbody');
+        var tableRowsClass = $('.table-list-search tbody tr');
+        $('.search-sf').remove();
+        tableRowsClass.each( function(i, val) {
+        
+            //Lower text for case insensitive
+            var rowText = $(val).text().toLowerCase();
+            var inputText = $(that).val().toLowerCase();
+            if(inputText != '')
+            {
+                $('.search-query-sf').remove();
+                tableBody.prepend('<tr class="search-query-sf"><td colspan="6"><strong>Searching for: "'
+                    + $(that).val()
+                    + '"</strong></td></tr>');
+            }
+            else
+            {
+                $('.search-query-sf').remove();
+            }
+
+            if( rowText.indexOf( inputText ) == -1 )
+            {
+                //hide rows
+                tableRowsClass.eq(i).hide();
+                
+            }
+            else
+            {
+                $('.search-sf').remove();
+                tableRowsClass.eq(i).show();
+            }
+        });
+        //all tr elements are hidden
+        if(tableRowsClass.children(':visible').length == 0)
+        {
+            tableBody.append('<tr class="search-sf"><td class="text-muted" colspan="6">No entries found.</td></tr>');
+        }
+    });
+});
 })();
