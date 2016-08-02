ddc0b6f9cc7f112294d690c93a20f11721f41f7d
Gytis Val. <gytis.valukonis@stud.viko.lt>
2016-08-02 16:32:12 +0300

    Added to solve CrossSite problem.

---
+++ BAChallengeWebServices/BAChallengeWebServices/Utility/AllowCrossSiteJson.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Filters;
+
+namespace BAChallengeWebServices.Utility
+{
+    public class AllowCrossSiteJson : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
+        {
+            if (actionExecutedContext.Response != null)
+                actionExecutedContext.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+
+            base.OnActionExecuted(actionExecutedContext);
+        }
+    }
+}
