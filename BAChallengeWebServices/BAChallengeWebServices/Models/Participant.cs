e10398910945ed58c0ede78e7c5b093856164cfe
Lukas <Studentas@STUDENTAS>
2016-08-03 00:50:03 -0700

    Participant, Result database tables

---
+++ BAChallengeWebServices/BAChallengeWebServices/Models/Participant.cs
@@ -0,0 +1,17 @@
+﻿using BAChallengeWebServices.Utility;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BAChallengeWebServices.Models
+{
+    public class Participants
+    {
+        public int ParticipantsId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
