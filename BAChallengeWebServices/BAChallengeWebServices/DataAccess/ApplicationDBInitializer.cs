4c22d15cfe95879a6897327cd41600be190a895b
Lukas Ignatavicius <Studentas@STUDENTAS>
2016-08-03 03:56:58 -0700

    Few Inits

--- BAChallengeWebServices/BAChallengeWebServices/DataAccess/ApplicationDBInitializer.cs
+++ BAChallengeWebServices/BAChallengeWebServices/DataAccess/ApplicationDBInitializer.cs
@@ -44,26 +44,16 @@ namespace BAChallengeWebServices.DataAccess
             //Participant Database table
             context.Participants.Add(new Participant { ParticipantId = 1, Name = "Nikolaj", Surname = "Anikejev", Results = new List<Result>
             {
-                new Result { ResultId = 1, Activity }
-            } });
-            /*context.Participants.Add(new Participant { ParticipantId = 2, Name = "Rimvydas", Surname = "Aniulis" });
-            context.Participants.Add(new Participant { ParticipantId = 3, Name = "Mindaugas", Surname = "Ardaravičius" });
-            context.Participants.Add(new Participant { ParticipantId = 4, Name = "Sandra", Surname = "Auzacalitaitė" });
-            context.Participants.Add(new Participant { ParticipantId = 5, Name = "Ignas", Surname = "Bagdonas" });
-            context.Participants.Add(new Participant { ParticipantId = 6, Name = "Vitalij", Surname = "Bakula" });
-            context.Participants.Add(new Participant { ParticipantId = 7, Name = "Laura", Surname = "Balkiūte" });
-            context.Participants.Add(new Participant { ParticipantId = 8, Name = "Irmantas", Surname = "Bankauskas" });
-            
-            //Result Database table
-            context.Results.Add(new Result { ResultId = 1, ParticipantId = 1, ActivityId = 1, Results = 8, Description = "Toli nubego"});
-            context.Results.Add(new Result { ResultId = 2, ParticipantId = 4, ActivityId = 1, Results = 4, Description = "Toli nubego" });
-            context.Results.Add(new Result { ResultId = 3, ParticipantId = 4, ActivityId = 2, Results = 6, Description = "Gerai padirbejo" });
-            context.Results.Add(new Result { ResultId = 4, ParticipantId = 5, ActivityId = 8, Results = 6, Description = "Ne paskutinis" });
-            context.Results.Add(new Result { ResultId = 5, ParticipantId = 6, ActivityId = 1, Results = 6, Description = "Labai Toli nubego" });
-            context.Results.Add(new Result { ResultId = 6, ParticipantId = 6, ActivityId = 2, Results = 8, Description = "Pasivijo Igna Bagdona" });
-            context.Results.Add(new Result { ResultId = 7, ParticipantId = 8, ActivityId = 1, Results = 6, Description = "Neblogai nubego" });
-            context.Results.Add(new Result { ResultId = 8, ParticipantId = 8, ActivityId = 17, Results = 2, Description = "Geras totalizatorius" });
-            context.Results.Add(new Result { ResultId = 9, ParticipantId = 8, ActivityId = 19, Results = 4, Description = "Toli nuskrido" });*/
+                new Result { ResultId = 1, Activity = new Activity { ActivityId = 1, Name = "We Run", Date = DateTime.ParseExact("2016-03-12 16:30", "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture), RegistrationDate = DateTime.ParseExact("2016-03-09 16:30", "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture), Branch = ActivityBranch.Sports, Status = ActivityStatus.Open, Description = "Blank", Location= "Vilnius", RegistrationUrl = @"https://docs.google.com/spreadsheets/d/1fb_OWYg_X-JGkTogEQe78qoakBh-H2UpFDr1OOjwlwM/edit?usp=sharing" }, Points = 6, Description = "Geras darbas" },
+                 new Result { ResultId = 2, Activity = new Activity { ActivityId = 2, Name = "Vilnius Challenge", Date = DateTime.ParseExact("2016-03-14 15:30", "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture), RegistrationDate = DateTime.ParseExact("2016-03-11 15:30", "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture), Branch = ActivityBranch.Sports, Status = ActivityStatus.Open, Description = "Blank", Location = "Vilnius", RegistrationUrl = @"https://docs.google.com/spreadsheets/d/1fb_OWYg_X-JGkTogEQe78qoakBh-H2UpFDr1OOjwlwM/edit?usp=sharing" }, Points = 4, Description = "Neblogai" },
+            }
+            } );
+            context.Participants.Add(new Participant { ParticipantId = 2, Name = "Rimvydas", Surname = "Aniulis", Results = new List<Result>
+            {
+                new Result { ResultId = 1, Activity = new Activity { ActivityId = 1, Name = "We Run", Date = DateTime.ParseExact("2016-03-12 16:30", "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture), RegistrationDate = DateTime.ParseExact("2016-03-09 16:30", "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture), Branch = ActivityBranch.Sports, Status = ActivityStatus.Open, Description = "Blank", Location= "Vilnius", RegistrationUrl = @"https://docs.google.com/spreadsheets/d/1fb_OWYg_X-JGkTogEQe78qoakBh-H2UpFDr1OOjwlwM/edit?usp=sharing" }, Points = 5, Description = "Gerai padirbejai" },
+                 new Result { ResultId = 2, Activity = new Activity { ActivityId = 2, Name = "Vilnius Challenge", Date = DateTime.ParseExact("2016-03-14 15:30", "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture), RegistrationDate = DateTime.ParseExact("2016-03-11 15:30", "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture), Branch = ActivityBranch.Sports, Status = ActivityStatus.Open, Description = "Blank", Location = "Vilnius", RegistrationUrl = @"https://docs.google.com/spreadsheets/d/1fb_OWYg_X-JGkTogEQe78qoakBh-H2UpFDr1OOjwlwM/edit?usp=sharing" }, Points = 2, Description = "Silpnai" },
+            }
+            });
         }
     }
 }
