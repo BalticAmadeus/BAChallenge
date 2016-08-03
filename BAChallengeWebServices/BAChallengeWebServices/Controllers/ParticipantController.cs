5012d2c02fd5a14a4993d1bf611a868a34e8f077
Studentas <Studentas@STUDENTAS>
2016-08-03 01:10:03 -0700

    Result Controller

--- BAChallengeWebServices/BAChallengeWebServices/Controllers/ParticipantController.cs
+++ BAChallengeWebServices/BAChallengeWebServices/Controllers/ParticipantController.cs
@@ -1,4 +1,6 @@
 ﻿using BAChallengeWebServices.DataAccess;
+using BAChallengeWebServices.Utility;
+using BAChallengeWebServices.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,7 @@ using System.Web.Http;
 
 namespace BAChallengeWebServices.Controllers
 {
+    [AllowCrossSiteJson]
     public class ParticipantController : ApiController
     {
         private ApplicationDBContext _dbContext;
@@ -18,7 +21,7 @@ namespace BAChallengeWebServices.Controllers
         }
         public IHttpActionResult Get()
         {
-            if (_dbContext.Participants.Count != 0)
+            if (_dbContext.Participants.Count() != 0)
             {
                 return Ok(_dbContext.Participants);
             }
@@ -28,10 +31,46 @@ namespace BAChallengeWebServices.Controllers
             }
         }
 
-        [Authorize]
-        public IHttpActionResult Put()
+        //[Authorize]
+        public IHttpActionResult Post([FromBody] Participant participant)
         {
 
+            if (_dbContext.Participants.Where(x => x.ParticipantId == participant.ParticipantId).Count() > 0)
+            {
+                return BadRequest();
+            }
+            _dbContext.Participants.Add(participant);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+
+        //[Authorize]
+        public IHttpActionResult Delete(int id)
+        {
+            var participant = _dbContext.Participants.FirstOrDefault(u => u.ParticipantId == id);
+            if (participant != null)
+            {
+                _dbContext.Participants.Remove(participant);
+                _dbContext.SaveChanges();
+
+                return Ok();
+            }
+
+            return NotFound();
+        }
+
+        public IHttpActionResult Put(int id, [FromBody]Participant participant)
+        {
+            var selectedParticipant = _dbContext.Participants.FirstOrDefault(u => u.ParticipantId == id);
+            if (selectedParticipant != null)
+            {
+                selectedParticipant.Name = participant.Name;
+                selectedParticipant.Surname = participant.Surname;
+                _dbContext.SaveChanges();
+                return Ok();
+            }
+            return BadRequest();
         }
     }
 }
---
+++ BAChallengeWebServices/BAChallengeWebServices/Controllers/ResultController.cs
@@ -0,0 +1,63 @@
+﻿using BAChallengeWebServices.Utility;
+using BAChallengeWebServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BAChallengeWebServices.DataAccess;
+
+namespace BAChallengeWebServices.Controllers
+{
+        [AllowCrossSiteJson]
+    public class ResultController : ApiController
+    {
+        private ApplicationDBContext _dbContext;
+
+        public ResultController()
+        {
+            _dbContext = new ApplicationDBContext();
+        }
+
+        public IHttpActionResult Get()
+        {
+            if (_dbContext.Results.Count() != 0)
+            {
+                return Ok(_dbContext.Results);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        public IHttpActionResult Post([FromBody] Result result)
+        {
+
+            if (_dbContext.Results.Where(x => x.ResultId == result.ResultId).Count() > 0 && result.Results >= 0)
+            {
+                return BadRequest();
+            }
+            _dbContext.Results.Add(result);
+            _dbContext.SaveChanges();
+
+            return Ok();
+        }
+
+        //[Authorize]
+        public IHttpActionResult Delete(int id)
+        {
+            var result = _dbContext.Results.FirstOrDefault(u => u.ResultId == id);
+            if (result != null)
+            {
+                _dbContext.Results.Remove(result);
+                _dbContext.SaveChanges();
+
+                return Ok();
+            }
+
+            return NotFound();
+        }
+    }
+}
